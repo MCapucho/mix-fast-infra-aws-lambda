be3dd78c1d61026da082d57c9ec705bd4bcb084b
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-25 12:04:25 -0300

    Implementação da Infra Lambda

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,15 @@
 output "lambda_invoke" {
   value = aws_lambda_function.mixfast_lambda_authorizer.invoke_arn
 }
+
+output "lambda_invoke_create" {
+  value = aws_lambda_function.mixfast_authorizer_cognito_create.invoke_arn
+}
+
+output "lambda_invoke_confirm" {
+  value = aws_lambda_function.mixfast_authorizer_cognito_confirm.invoke_arn
+}
+
+output "lambda_invoke_login" {
+  value = aws_lambda_function.mixfast_authorizer_cognito_login.invoke_arn
+}
